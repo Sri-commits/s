9ade25f59dc8960956015f21fa8bdd6d0327adc1
Sri-commits <sriparna.jis.ece@gmail.com>
2021-05-14 23:44:53 +0530

    commit on master

---
+++ src/app/teachers/teachers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sch-teachers',
+  templateUrl: './teachers.component.html',
+  styleUrls: ['./teachers.component.css']
+})
+export class TeachersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
